1e7549f78fb35d4b1353df2e243ef7704ed9f222
Kiran Chenna <chenna.kiran1@gmail.com>
2017-05-09 16:12:44 +0530

    Updated New Code
    Added Gio Locatio

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,17 +7,23 @@ import { Geolocation } from '@ionic-native/geolocation';
 })
 export class HomePage {
   watch: any;
+  lat:any = 0.00;
+  lon:any = 0.00;
   constructor(public navCtrl: NavController, private geolocation: Geolocation) {
     this.geolocation.getCurrentPosition().then((resp) => {
       // resp.coords.latitude
       // resp.coords.longitude
       console.log(resp.coords.latitude, resp.coords.longitude);
+      this.lat = resp.coords.latitude;
+      this.lon = resp.coords.longitude;
     }).catch((error) => {
       console.log('Error getting location', error);
     });
     this.watch = this.geolocation.watchPosition();
     this.watch.subscribe((resp) => {
-      console.log(resp.coords.latitude, resp.coords.longitude);
+      console.log(resp);
+      this.lat = resp.coords.latitude;
+      this.lon = resp.coords.longitude;
     });
   }
 
